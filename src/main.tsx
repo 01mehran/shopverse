af605c979366625bd9cd8902c5d2bfeab78afbaf
mehran <mehranmohamadi1311@gmail.com>
2026-08-05 23:55:22 +0330

    docs(tailwindcss): install tailwindcss for project styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// React;
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+// Components;
+import App from "./App.tsx";
+
+//Global Css;
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
