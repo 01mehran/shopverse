889a4223405c1eef46f71b969cbc6e6d042dce21
mehran <mehranmohamadi1311@gmail.com>
2026-08-07 02:31:25 +0330

    feat(hero): add hero section laptap screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 // Components;
 import TopBar from "@components/TopBar";
 import Header from "@components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <section>
-      <TopBar />
-      <Header />
+      <div className="min-h-screen grid grid-rows-[auto_auto_1fr]">
+        <TopBar />
+        <Header />
+        <Hero />
+      </div>
     </section>
   );
 }
