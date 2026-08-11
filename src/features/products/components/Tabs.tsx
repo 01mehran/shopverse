361d4065b7f6ac1584d088d9e7a58ccc4e888a74
mehran <mehranmohamadi1311@gmail.com>
2026-08-11 23:42:35 +0330

    feat(product-details): add tab layout

---
+++ src/features/products/components/Tabs.tsx
@@ -0,0 +1,18 @@
+import { Container } from "@/shared/components";
+
+export default function Tabs() {
+  return (
+    <section className="mt-4 py-12">
+      <Container>
+        <div className="font-Satoshi relative flex items-center justify-around border-b border-b-black/10 py-2 text-sm text-black/60 md:text-base">
+          {/* Indicator */}
+          <div className="absolute bottom-0 left-1/2 h-0.5 w-[calc(100%/3-35px)] -translate-x-1/2 bg-black"></div>
+
+          <p>Product Details</p>
+          <p className="font-medium text-black">Rating & Reviews</p>
+          <p>FAQs</p>
+        </div>
+      </Container>
+    </section>
+  );
+}
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,6 +1,7 @@
 // Components;
 import { BreadCrumb, Header, TopBar } from "@/shared/components";
 import ProductInfo from "@/features/products/components/ProductInfo";
+import Tabs from "@/features/products/components/Tabs";
 
 export default function ProductDetails() {
   return (
@@ -9,6 +10,7 @@ export default function ProductDetails() {
       <Header />
       <BreadCrumb />
       <ProductInfo />
+      <Tabs />
     </section>
   );
 }
