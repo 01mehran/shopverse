a88ea1943390330eee6a26b39f335d901b801727
mehran <mehranmohamadi1311@gmail.com>
2026-08-12 02:22:13 +0330

    feat(cart): add header component Closes #14

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,12 @@
 // Components;
-import { TopBar } from "@/shared/components";
+import { BreadCrumb, Header, TopBar } from "@/shared/components";
 
 export default function Cart() {
   return (
     <section>
       <TopBar />
+      <Header />
+      <BreadCrumb />
     </section>
   );
 }
