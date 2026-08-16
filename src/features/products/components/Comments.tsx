854976d125d217e0e96360b2d6a05231b311f925
mehran <mehranmohamadi1311@gmail.com>
2026-08-16 15:16:31 +0330

    feat(product-info): add motion animations to tabs, user comments

--- src/features/products/components/Comments.tsx
+++ src/features/products/components/Comments.tsx
@@ -5,6 +5,16 @@ import Tabs from "./Tabs";
 // Static Images;
 import ratingStar from "@/assets/images/home/rating-start.png";
 
+// Motion;
+import { motion } from "motion/react";
+
+// Animations;
+import {
+  fadeLeftVariant,
+  productCardContainerVariants,
+  productCardVariants,
+} from "@/shared/animations";
+
 // Icons;
 import { Ellipsis, SlidersVertical } from "lucide-react";
 
@@ -14,7 +24,13 @@ export default function Comments() {
       <Container>
         <Tabs />
 
-        <header className="flex items-center justify-between">
+        <motion.header
+          initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          className="flex items-center justify-between"
+        >
           <h1 className="text-base font-bold md:text-2xl">
             All Reviews{" "}
             <span className="text-sm font-normal text-black/60 md:text-base">
@@ -34,14 +50,21 @@ export default function Comments() {
               Write a Review
             </button>
           </div>
-        </header>
+        </motion.header>
 
         <main className="mt-4 md:mt-8">
           {/* User Comments */}
           <section>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <motion.div
+              variants={productCardContainerVariants}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              className="grid grid-cols-1 gap-4 md:grid-cols-2"
+            >
               {Array.from({ length: 4 }, (_, i) => (
-                <article
+                <motion.article
+                  variants={productCardVariants}
                   key={i}
                   className="relative flex flex-col gap-2 rounded-2xl border border-black/10 p-4 shadow md:justify-between md:p-7"
                 >
@@ -62,14 +85,20 @@ export default function Comments() {
                   <button className="absolute top-6 right-6 hidden cursor-pointer text-black/40 md:block">
                     <Ellipsis />
                   </button>
-                </article>
+                </motion.article>
               ))}
-            </div>
+            </motion.div>
 
             {/* Load More Comments Button */}
-            <button className="mx-auto mt-5 block w-50 cursor-pointer rounded-[62px] border border-black/10 py-2 text-sm font-medium sm:mt-10 sm:text-base">
+            <motion.button
+              variants={fadeLeftVariant}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              className="mx-auto mt-5 block w-50 cursor-pointer rounded-[62px] border border-black/10 py-2 text-sm font-medium sm:mt-10 sm:text-base"
+            >
               Load More Reviews
-            </button>
+            </motion.button>
           </section>
         </main>
       </Container>
--- src/features/products/components/Tabs.tsx
+++ src/features/products/components/Tabs.tsx
@@ -1,18 +1,26 @@
 // Components;
 import { Container } from "@/shared/components";
 
+import { motion } from "motion/react";
+
 export default function Tabs() {
   return (
     <section className="mt-4 py-12">
       <Container>
-        <div className="relative flex items-center justify-between border-b border-b-black/10 py-2 text-sm text-black/60 md:justify-around md:text-base">
+        <motion.div
+          initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6, delay: 0.15 }}
+          className="relative flex items-center justify-between border-b border-b-black/10 py-2 text-sm text-black/60 md:justify-around md:text-base"
+        >
           {/* Indicator */}
           <div className="absolute bottom-0 left-1/2 h-0.5 w-[calc(100%/3-35px)] -translate-x-1/2 bg-black"></div>
 
           <p>Product Details</p>
           <p className="font-medium text-black">Rating & Reviews</p>
           <p>FAQs</p>
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
