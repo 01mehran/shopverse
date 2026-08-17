9e29215939ec42f6acc48c8d2c12ffd350d3e9b0
mehran <mehranmohamadi1311@gmail.com>
2026-08-17 15:42:16 +0330

    refactor(footer): extract footer bottom to separate compoennt

--- src/shared/components/footer/Footer.tsx
+++ src/shared/components/footer/Footer.tsx
@@ -2,8 +2,8 @@
 import Container from "../Container";
 import FooterColumn from "./FooterColumn";
 import FooterLogo from "./FooterLogo";
-import FooterLogosBox from "./FooterlogosBox";
 import NewsletterSignup from "./NewsletterSignup";
+import FooterBottom from "./FooterBottom";
 
 // Motion Component;
 import { motion } from "motion/react";
@@ -13,7 +13,6 @@ import { productCardContainerVariants } from "@/shared/animations";
 
 // Data;
 import { footerLinks } from "./data/footerLinks";
-import { footerImages } from "./data/footerPaymentLogos";
 
 export default function Footer() {
   return (
@@ -46,28 +45,8 @@ export default function Footer() {
 
           <hr className="border border-black/5" />
 
-          {/* Under Footer */}
-          <motion.section
-            initial={{ scale: 0.5, opacity: 0 }}
-            whileInView={{ scale: 1, opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay: 0.3 }}
-            className="flex flex-col items-center justify-between space-y-4 pt-8 md:flex-row"
-          >
-            <p className="text-base text-black/60">
-              Shop.co © 2000-2023, All Rights Reserved
-            </p>
-            <div className="flex items-center gap-3">
-              {footerImages.map((images) => (
-                <FooterLogosBox
-                  imageUrl={images.imageUrl}
-                  key={images.alt}
-                  alt={images.alt}
-                  className="shadow-sm"
-                />
-              ))}
-            </div>
-          </motion.section>
+          {/* Footer Bottom */}
+          <FooterBottom />
         </Container>
       </main>
     </footer>
---
+++ src/shared/components/footer/FooterBottom.tsx
@@ -0,0 +1,31 @@
+// Compoennts;
+import FooterLogosBox from "./FooterlogosBox";
+// Motion Component;
+import { motion } from "motion/react";
+import { footerImages } from "./data/footerPaymentLogos";
+
+export default function FooterBottom() {
+  return (
+    <motion.section
+      initial={{ scale: 0.5, opacity: 0 }}
+      whileInView={{ scale: 1, opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay: 0.3 }}
+      className="flex flex-col items-center justify-between space-y-4 pt-8 md:flex-row"
+    >
+      <p className="text-base text-black/60">
+        Shop.co © 2000-2023, All Rights Reserved
+      </p>
+      <div className="flex items-center gap-3">
+        {footerImages.map((images) => (
+          <FooterLogosBox
+            imageUrl={images.imageUrl}
+            key={images.alt}
+            alt={images.alt}
+            className="shadow-sm"
+          />
+        ))}
+      </div>
+    </motion.section>
+  );
+}
