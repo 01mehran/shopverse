c7a23b58c15ae327cd07de37ade63fc40de2ebdf
mehran <mehranmohamadi1311@gmail.com>
2026-08-17 14:23:58 +0330

    refactor(top-selling): extract data to data folder

--- src/features/home/components/TopSellingList.tsx
+++ src/features/home/components/TopSellingList.tsx
@@ -4,11 +4,8 @@ import { Container, ProductCard } from "@/shared/components";
 // Motion Component;
 import { motion } from "motion/react";
 
-// Static Images;
-import pic5 from "@images/home/pic5.png";
-import pic6 from "@images/home/pic6.png";
-import pic7 from "@images/home/pic7.png";
-import pic8 from "@images/home/pic8.png";
+// Data;
+import { topSellingProducts } from "../data/topSelling";
 
 // Animations;
 import {
@@ -17,38 +14,6 @@ import {
   productCardVariants,
   fadeLeftVariant,
 } from "@/shared/animations";
-
-// Static Data;
-const products = [
-  {
-    id: 1,
-    image: pic5,
-    name: "Vertical Striped Shirt",
-    rating: 4.5,
-    price: 29,
-  },
-  {
-    id: 2,
-    image: pic6,
-    name: "Courage Graphic T-shirt",
-    rating: 4.2,
-    price: 45,
-  },
-  {
-    id: 3,
-    image: pic7,
-    name: "Loose Fit Bermuda Shorts",
-    rating: 4.8,
-    price: 79,
-  },
-  {
-    id: 4,
-    image: pic8,
-    name: "Faded Skiny Jneas",
-    rating: 4.6,
-    price: 65,
-  },
-];
 
 export default function TopSellingList() {
   return (
@@ -72,7 +37,7 @@ export default function TopSellingList() {
           viewport={{ once: true }}
           className="hide-scrollbar flex items-baseline justify-between gap-3 overflow-x-auto py-4"
         >
-          {products.map((item) => (
+          {topSellingProducts.map((item) => (
             <motion.div key={item.id} variants={productCardVariants}>
               <ProductCard key={item.id} item={item} />
             </motion.div>
---
+++ src/features/home/data/topSelling.ts
@@ -0,0 +1,36 @@
+// Static Images;
+import pic5 from "@images/home/pic5.png";
+import pic6 from "@images/home/pic6.png";
+import pic7 from "@images/home/pic7.png";
+import pic8 from "@images/home/pic8.png";
+
+export const topSellingProducts = [
+  {
+    id: 1,
+    image: pic5,
+    name: "Vertical Striped Shirt",
+    rating: 4.5,
+    price: 29,
+  },
+  {
+    id: 2,
+    image: pic6,
+    name: "Courage Graphic T-shirt",
+    rating: 4.2,
+    price: 45,
+  },
+  {
+    id: 3,
+    image: pic7,
+    name: "Loose Fit Bermuda Shorts",
+    rating: 4.8,
+    price: 79,
+  },
+  {
+    id: 4,
+    image: pic8,
+    name: "Faded Skiny Jneas",
+    rating: 4.6,
+    price: 65,
+  },
+];
