b4dee6d870c9db5af8e69f825bf347e2ef49a04c
mehran <mehranmohamadi1311@gmail.com>
2026-08-17 14:06:14 +0330

    refactor(brand-logos): render images from dtat

--- src/features/home/components/BrandLogos.tsx
+++ src/features/home/components/BrandLogos.tsx
@@ -4,12 +4,8 @@ import { Container } from "@/shared/components";
 // Motion Component;
 import { motion } from "motion/react";
 
-// Logos
-import calvinKlein from "@images/home/brand-logos/calvin-klein-logo.svg";
-import gucci from "@images/home/brand-logos/gucci-logo.svg";
-import prada from "@images/home/brand-logos/prada-logo.svg";
-import versace from "@images/home/brand-logos/versace-logo.svg";
-import zara from "@images/home/brand-logos/zara-logo.svg";
+// Data;
+import { logos } from "../data/logos";
 
 export default function BrandLogos() {
   return (
@@ -21,16 +17,15 @@ export default function BrandLogos() {
       className="bg-black"
     >
       <Container>
-        <div className="flex flex-wrap items-center justify-evenly gap-6 py-6 sm:py-8 md:justify-between">
-          <img src={versace} alt="versace logo" className="w-20 md:w-32" />
-          <img src={zara} alt=" zara logo" className="w-16 md:w-24" />
-          <img src={gucci} alt=" gucci logo" className="w-20 md:w-32" />
-          <img src={prada} alt="prada logo" className="w-20 md:w-32" />
-          <img
-            src={calvinKlein}
-            alt="calvin Klein logo"
-            className="w-20 md:w-32"
-          />
+        <div className="flex flex-wrap items-center justify-evenly gap-6 py-6 text-white sm:py-8 md:justify-between">
+          {logos.map((logo) => (
+            <img
+              src={logo.image}
+              key={logo.id}
+              alt={logo.alt}
+              className="w-20 md:w-32"
+            />
+          ))}
         </div>
       </Container>
     </motion.section>
---
+++ src/features/home/data/logos.ts
@@ -0,0 +1,34 @@
+// Static Images;
+import calvinKlein from "@images/home/brand-logos/calvin-klein-logo.svg";
+import gucci from "@images/home/brand-logos/gucci-logo.svg";
+import prada from "@images/home/brand-logos/prada-logo.svg";
+import versace from "@images/home/brand-logos/versace-logo.svg";
+import zara from "@images/home/brand-logos/zara-logo.svg";
+
+export const logos = [
+  {
+    id: 1,
+    alt: "calvinKlein logo",
+    image: calvinKlein,
+  },
+  {
+    id: 2,
+    alt: "gucci logo",
+    image: gucci,
+  },
+  {
+    id: 3,
+    alt: "prada logo",
+    image: prada,
+  },
+  {
+    id: 4,
+    alt: "versace logo",
+    image: versace,
+  },
+  {
+    id: 5,
+    alt: "zara logo",
+    image: zara,
+  },
+];
