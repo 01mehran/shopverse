916fae6667d6331e761f12bd5b3b418da2c869fb
mehran <mehranmohamadi1311@gmail.com>
2026-09-02 22:55:55 +0330

    feat(header-icons): add cart badge layout

--- src/shared/components/header/HeaderIcons.tsx
+++ src/shared/components/header/HeaderIcons.tsx
@@ -14,8 +14,13 @@ export default function HeaderIcons() {
     <section className="flex items-baseline gap-2.5">
       {/* Cart Icon; */}
       <Link to={`/cart`}>
-        <button className="cursor-pointer focus-within:border-0 hover:text-black/80">
+        <button className="relative cursor-pointer focus-within:border-0 hover:text-black/80">
           <ShoppingCart strokeWidth={2.5} />
+
+          {/* Cart Badge  */}
+          <span className="absolute -top-2 -right-2 flex size-5 items-center justify-center rounded-full bg-black text-[10px] font-medium text-white">
+            1
+          </span>
         </button>
       </Link>
 
