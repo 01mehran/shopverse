333b9a5df444f67792b20fe8685d734a1a6f3bda
mehran <mehranmohamadi1311@gmail.com>
2026-09-17 16:20:38 +0330

    feat(ordersummary): implement total price Closes #82

--- src/features/cart/components/OrderSummary.tsx
+++ src/features/cart/components/OrderSummary.tsx
@@ -43,6 +43,9 @@ export default function OrderSummary() {
     return total + (product?.price ?? 0) * cartItem.quantity;
   }, 0);
 
+  const deliveryFee = 15;
+  const total = subtotal + deliveryFee;
+
   return (
     <article className="border-bg-muted col-span-5 rounded-2xl border p-2.5 md:p-5">
       <motion.div
@@ -76,18 +79,20 @@ export default function OrderSummary() {
         {/* Delivery Fee */}
         <div className="flex items-center justify-between">
           <span className="text-base font-normal text-black/60 md:text-lg">
-            Delivery Fee{" "}
+            Delivery Fee
           </span>
-          <span className="text-base font-bold md:text-lg">$15</span>
+          <span className="text-base font-bold md:text-lg">${deliveryFee}</span>
         </div>
 
         {/* Total */}
         <hr className="border border-black/5" />
         <div className="flex items-center justify-between">
           <span className="text-base font-normal text-black/80 md:text-lg">
-            Total{" "}
+            Total
           </span>
-          <span className="text-base font-bold md:text-xl">$515</span>
+          <span className="text-base font-bold md:text-xl">
+            {isLoading ? <Loading size={18} /> : `$${total}`}
+          </span>
         </div>
 
         {/* Form */}
@@ -96,13 +101,16 @@ export default function OrderSummary() {
             placeholder="Add promo code"
             className="bg-bg-muted col-span-6 rounded-[62px] px-5 py-1.5 placeholder:text-sm"
           />
-          <button className="col-span-3 cursor-pointer rounded-[62px] bg-black text-sm text-white lg:text-base">
+          <button
+            type="button"
+            className="col-span-3 cursor-pointer rounded-[62px] bg-black text-sm text-white lg:text-base"
+          >
             Apply
           </button>
         </form>
 
         <button className="mt-2 flex w-full cursor-pointer items-center justify-center rounded-[62px] bg-black py-2 text-center text-white">
-          Go to Checkout{" "}
+          <span>Go to Checkout</span>
           <span className="ml-3">
             <ArrowRight size={20} />
           </span>
